ac89aa58f7fd075b79f5d1ff3f4c199feecb41f8
krish-08082 <ai.everything.updates@gmail.com>
2026-08-08 21:25:01 +0530

    docs: here is  my second module that is variables

---
+++ 02-variables/COMMANDS.md
@@ -0,0 +1,14 @@
+variable assignment
+variable expansion
+$(command)
+echo "$?"
+echo "$HOME"
+echo "$USER"
+echo "$PWD"
+echo "$PATH"
+which
+command -v
+$1
+$2
+$#
+$@
---
+++ 02-variables/lab.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+echo "------------------------------------------------"
+echo "ENVIRONMENT REPORT"
+echo "---------------------------------------------"
+
+project="Bash-scripting"
+
+echo "Project : $project"
+
+date=$(date)
+echo "Date : $date"
+
+
+cd "$HOME"
+
+echo "$USER"
+echo "Running as : $USER"
